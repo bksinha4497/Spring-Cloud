35d7a7f683a909763d8638d497fe417e1fee3fcf
Biswajit Kumar <bksinha4497@gmail.com>
2021-12-28 00:04:15 +0530

    Added spring cloud zookeeper dependency

--- SpringCloudStreamKafka/src/main/java/com/bk/cloud/SpringCloudStreamKafkaApplication.java
+++ SpringCloudStreamKafka/src/main/java/com/bk/cloud/SpringCloudStreamKafkaApplication.java
@@ -2,11 +2,12 @@ package com.bk.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import lombok.extern.slf4j.Slf4j;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-@Slf4j
 public class SpringCloudStreamKafkaApplication {
 
 	public static void main(String[] args) {
