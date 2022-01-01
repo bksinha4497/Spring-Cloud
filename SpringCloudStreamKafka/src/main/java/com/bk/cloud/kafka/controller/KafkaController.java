0effd7c46a6fdf4a44e0ec54322bf2a231506af8
Biswajit Kumar <bksinha4497@gmail.com>
2022-01-01 11:29:20 +0530

    Update response for kafka controller

--- SpringCloudStreamKafka/src/main/java/com/bk/cloud/kafka/controller/KafkaController.java
+++ SpringCloudStreamKafka/src/main/java/com/bk/cloud/kafka/controller/KafkaController.java
@@ -3,6 +3,7 @@ package com.bk.cloud.kafka.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,10 +20,10 @@ public class KafkaController {
 	private StreamBridge publisher;
 
 	@PostMapping(value = "/publish", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getData(@RequestBody KafkaDetails object) {
+	public ResponseEntity<String> getData(@RequestBody KafkaDetails object) {
 		log.info("Recieved from controller - {} ", object);
 		publisher.send("producer-out-0", object);
-		return "published";
+		return ResponseEntity.ok().body("{\"RESPONSE\":\"SUCCESS\"}");
 	}
 
 
--- SpringCloudStreamKafka/src/main/java/com/bk/cloud/kafka/model/KafkaDetails.java
+++ SpringCloudStreamKafka/src/main/java/com/bk/cloud/kafka/model/KafkaDetails.java
@@ -1,8 +1,12 @@
 package com.bk.cloud.kafka.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class KafkaDetails {
 
 	private String topicName;
